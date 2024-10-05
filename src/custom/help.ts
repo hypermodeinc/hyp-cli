f8b92a6462e1cecd8654715a2bb32bcbeefaec43
Jai Radhakrishnan <55522316+jairad26@users.noreply.github.com>
2024-10-04 17:47:13 -0700

    fix npm run build

--- src/custom/help.ts
+++ src/custom/help.ts
@@ -4,9 +4,47 @@ import chalk from 'chalk'
 const CLI_VERSION = '0.0.0'
 
 export default class CustomHelp extends Help {
-  private target_pad = 15
-  private pre_pad = 0
-  private post_pad = 0
+  private postPad = 0
+  private prePad = 0
+  private targetPad = 15
+  formatCommand(command: Command.Loadable): string {
+    let out = ''
+    out += chalk.bold('Usage:') + ' ' + chalk.bold.blueBright('hyp') + ' ' + command.id
+    return out
+  }
+
+  formatCommands(commands: Command.Loadable[]): string {
+    let out = ''
+    out += chalk.bold('Commands:') + '\n'
+
+    for (const command of commands) {
+      if (command.id === 'autocomplete') continue
+
+      const rawName = command.id.includes(':') ? command.id.split(':')[1] : command.id
+      const name = chalk.bold.blueBright(rawName)
+      const prePadding = ' '.repeat(Math.max(1, this.prePad - rawName.length))
+      const args =
+        Object.keys(command.args).length > 0
+          ? Object.entries(command.args)
+              .map((v) => {
+                if (!v[1].hidden && v[1].required && v[1].description && v[1].description.indexOf('-|-') > 0) {
+                    return v[1].description.split('-|-')[0]
+                  }
+
+                return ''
+              })
+              .join(' ')
+          : ''
+      const postPadding = ' '.repeat(Math.max(1, this.postPad - args.length))
+      const description = command.description!
+      const aliases = command.aliases.length > 0 ? chalk.dim(' (' + command.aliases.join('/') + ')') : ''
+
+      out += '  ' + name + prePadding + chalk.dim(args) + postPadding + description + aliases + '\n'
+    }
+
+    return out.trim()
+  }
+
   formatRoot(): string {
     let out = ''
     out +=
@@ -24,42 +62,18 @@ export default class CustomHelp extends Help {
     return out
   }
 
-  formatCommand(command: Command.Loadable): string {
+  formatRootFooter(): string {
     let out = ''
-    out += chalk.bold('Usage:') + ' ' + chalk.bold.blueBright('hyp') + ' ' + command.id
+    out +=
+      'View the docs:' +
+      ' '.repeat(Math.max(1, this.prePad + this.postPad - 12)) +
+      chalk.blueBright('https://docs.hypermode.com/introduction') +
+      '\n'
+    // out += "View the repo:" + " ".repeat(Math.max(1, this.pre_pad + this.post_pad - 12)) + chalk.blueBright("https://github.com/HypermodeInc/modus") + "\n";
+
+    out += '\n'
+    out += 'Made with 💖 by ' + chalk.magentaBright('https://hypermode.com/')
     return out
-  }
-
-  formatCommands(commands: Command.Loadable[]): string {
-    let out = ''
-    out += chalk.bold('Commands:') + '\n'
-
-    for (const command of commands) {
-      if (command.id === 'autocomplete') continue
-
-      const rawName = command.id.includes(':') ? command.id.split(':')[1] : command.id
-      const name = chalk.bold.blueBright(rawName)
-      const prePadding = ' '.repeat(Math.max(1, this.pre_pad - rawName.length))
-      const args =
-        Object.keys(command.args).length > 0
-          ? Object.entries(command.args)
-              .map((v) => {
-                if (!v[1].hidden && v[1].required) {
-                  if (v[1].description && v[1].description.indexOf('-|-') > 0) {
-                    return v[1].description.split('-|-')[0]
-                  }
-                }
-                return ''
-              })
-              .join(' ')
-          : ''
-      const postPadding = ' '.repeat(Math.max(1, this.post_pad - args.length))
-      const description = command.description!
-      const aliases = command.aliases.length > 0 ? chalk.dim(' (' + command.aliases.join('/') + ')') : ''
-
-      out += '  ' + name + prePadding + chalk.dim(args) + postPadding + description + aliases + '\n'
-    }
-    return out.trim()
   }
 
   formatTopic(topic: Interfaces.Topic): string {
@@ -73,7 +87,7 @@ export default class CustomHelp extends Help {
 
   formatTopics(topics: Interfaces.Topic[]): string {
     let out = ''
-    if (topics.find((v) => !v.hidden)) out += chalk.bold('Tools:') + '\n'
+    if (topics.some((v) => !v.hidden)) out += chalk.bold('Tools:') + '\n'
     else return out
 
     for (const topic of topics) {
@@ -81,108 +95,12 @@ export default class CustomHelp extends Help {
       out +=
         '  ' +
         chalk.bold.blue(topic.name) +
-        ' '.repeat(Math.max(1, this.pre_pad + this.post_pad - topic.name.length)) +
+        ' '.repeat(Math.max(1, this.prePad + this.postPad - topic.name.length)) +
         topic.description +
         '\n'
     }
+
     return out.trim()
-  }
-
-  formatRootFooter(): string {
-    let out = ''
-    out +=
-      'View the docs:' +
-      ' '.repeat(Math.max(1, this.pre_pad + this.post_pad - 12)) +
-      chalk.blueBright('https://docs.hypermode.com/introduction') +
-      '\n'
-    // out += "View the repo:" + " ".repeat(Math.max(1, this.pre_pad + this.post_pad - 12)) + chalk.blueBright("https://github.com/HypermodeInc/modus") + "\n";
-
-    out += '\n'
-    out += 'Made with 💖 by ' + chalk.magentaBright('https://hypermode.com/')
-    return out
-  }
-
-  async showRootHelp(): Promise<void> {
-    let rootTopics = this.sortedTopics
-    let rootCommands = this.sortedCommands
-    const state = this.config.pjson?.oclif?.state
-    if (state) {
-      this.log(state === 'deprecated' ? `${this.config.bin} is deprecated` : `${this.config.bin} is in ${state}.\n`)
-    }
-    this.log(this.formatRoot())
-    this.log('')
-    if (!this.opts.all) {
-      rootTopics = rootTopics.filter((t) => !t.name.includes(':'))
-      rootCommands = rootCommands.filter((c) => !c.id.includes(':'))
-    }
-
-    for (const command of rootCommands) {
-      if (command.id.length > this.pre_pad) this.pre_pad = command.id.length
-      const args =
-        Object.keys(command.args).length > 0
-          ? Object.entries(command.args)
-              .map((v) => {
-                if (!v[1].hidden && v[1].required) {
-                  if (v[1].description && v[1].description.indexOf('-|-') > 0) {
-                    return v[1].description.split('-|-')[0]
-                  }
-                }
-                return ''
-              })
-              .join(' ')
-          : ''
-      if (args.length > this.post_pad) this.post_pad = args.length
-    }
-    this.post_pad =
-      6 + this.pre_pad + this.post_pad > this.target_pad
-        ? 6 + this.pre_pad + this.post_pad - this.target_pad
-        : this.target_pad - this.pre_pad
-    this.pre_pad += 2
-
-    if (rootTopics.length > 0) {
-      this.log(this.formatTopics(rootTopics))
-      this.log('')
-    }
-    if (rootCommands.length > 0) {
-      rootCommands = rootCommands.filter((c) => c.id)
-      this.log(this.formatCommands(rootCommands))
-      this.log('')
-    }
-    this.log(this.formatRootFooter())
-  }
-
-  async showTopicHelp(topic: Interfaces.Topic) {
-    const {name} = topic
-    const commands = this.sortedCommands.filter((c) => c.id.startsWith(name + ':'))
-    for (const command of commands) {
-      if (command.id.split(':')[1].length > this.pre_pad) this.pre_pad = command.id.split(':')[1].length
-      const args =
-        Object.keys(command.args).length > 0
-          ? Object.entries(command.args)
-              .map((v) => {
-                if (!v[1].hidden && v[1].required) {
-                  if (v[1].description && v[1].description.indexOf('-|-') > 0) {
-                    return v[1].description.split('-|-')[0]
-                  }
-                }
-                return ''
-              })
-              .join(' ')
-          : ''
-      if (args.length > this.post_pad) this.post_pad = args.length
-    }
-    this.post_pad =
-      6 + this.pre_pad + this.post_pad > this.target_pad
-        ? 6 + this.pre_pad + this.post_pad - this.target_pad
-        : this.target_pad - this.pre_pad
-    this.pre_pad += 2
-    const state = this.config.pjson?.oclif?.state
-    if (state) this.log(`This topic is in ${state}.\n`)
-    this.log(this.formatTopic(topic))
-    if (commands.length > 0) {
-      this.log(this.formatCommands(commands))
-      this.log('')
-    }
   }
 
   async showCommandHelp(command: Command.Loadable): Promise<void> {
@@ -209,7 +127,7 @@ export default class CustomHelp extends Help {
     //     for (const example of examples) this.log("  " + chalk.dim(example));
     // }
 
-    if (flags.length) {
+    if (flags.length > 0) {
       this.log(chalk.bold('Flags:'))
       for (const flag of Object.values(command.flags))
         this.log(
@@ -217,18 +135,102 @@ export default class CustomHelp extends Help {
         )
     }
 
-    if (args.length) {
+    if (args.length > 0) {
       this.log(chalk.bold('Args:'))
-      for (let arg of Object.values(command.args)) {
-        let usage = ''
+      for (const arg of Object.values(command.args)) {
         let desc = arg.description
         if (arg.description?.includes('-|-')) {
-          usage = arg.description.split('-|-')[0]
           desc = arg.description.split('-|-')[1]
         }
 
         this.log('  ' + chalk.bold.blueBright(arg.name) + ' '.repeat(Math.max(1, margin + 2 - arg.name.length)) + desc)
       }
+    }
+  }
+
+  async showRootHelp(): Promise<void> {
+    let rootTopics = this.sortedTopics
+    let rootCommands = this.sortedCommands
+    const state = this.config.pjson?.oclif?.state
+    if (state) {
+      this.log(state === 'deprecated' ? `${this.config.bin} is deprecated` : `${this.config.bin} is in ${state}.\n`)
+    }
+
+    this.log(this.formatRoot())
+    this.log('')
+    if (!this.opts.all) {
+      rootTopics = rootTopics.filter((t) => !t.name.includes(':'))
+      rootCommands = rootCommands.filter((c) => !c.id.includes(':'))
+    }
+
+    for (const command of rootCommands) {
+      if (command.id.length > this.prePad) this.prePad = command.id.length
+      const args =
+        Object.keys(command.args).length > 0
+          ? Object.entries(command.args)
+              .map((v) => {
+                if (!v[1].hidden && v[1].required && v[1].description && v[1].description.indexOf('-|-') > 0) {
+                    return v[1].description.split('-|-')[0]
+                  }
+
+                return ''
+              })
+              .join(' ')
+          : ''
+      if (args.length > this.postPad) this.postPad = args.length
+    }
+
+    this.postPad =
+      6 + this.prePad + this.postPad > this.targetPad
+        ? 6 + this.prePad + this.postPad - this.targetPad
+        : this.targetPad - this.prePad
+    this.prePad += 2
+
+    if (rootTopics.length > 0) {
+      this.log(this.formatTopics(rootTopics))
+      this.log('')
+    }
+
+    if (rootCommands.length > 0) {
+      rootCommands = rootCommands.filter((c) => c.id)
+      this.log(this.formatCommands(rootCommands))
+      this.log('')
+    }
+
+    this.log(this.formatRootFooter())
+  }
+
+  async showTopicHelp(topic: Interfaces.Topic) {
+    const {name} = topic
+    const commands = this.sortedCommands.filter((c) => c.id.startsWith(name + ':'))
+    for (const command of commands) {
+      if (command.id.split(':')[1].length > this.prePad) this.prePad = command.id.split(':')[1].length
+      const args =
+        Object.keys(command.args).length > 0
+          ? Object.entries(command.args)
+              .map((v) => {
+                if (!v[1].hidden && v[1].required && v[1].description && v[1].description.indexOf('-|-') > 0) {
+                    return v[1].description.split('-|-')[0]
+                  }
+
+                return ''
+              })
+              .join(' ')
+          : ''
+      if (args.length > this.postPad) this.postPad = args.length
+    }
+
+    this.postPad =
+      6 + this.prePad + this.postPad > this.targetPad
+        ? 6 + this.prePad + this.postPad - this.targetPad
+        : this.targetPad - this.prePad
+    this.prePad += 2
+    const state = this.config.pjson?.oclif?.state
+    if (state) this.log(`This topic is in ${state}.\n`)
+    this.log(this.formatTopic(topic))
+    if (commands.length > 0) {
+      this.log(this.formatCommands(commands))
+      this.log('')
     }
   }
 }
