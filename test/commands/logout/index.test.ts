3549d77c29f40597ca9059a92f3ed6772539b55f
Jai Radhakrishnan <55522316+jairad26@users.noreply.github.com>
2024-10-23 10:05:47 -0700

    add release changes for hyp cli

--- test/commands/logout/index.test.ts
+++
@@ -1,9 +0,0 @@
-import {runCommand} from '@oclif/test'
-import {expect} from 'chai'
-
-describe('logout', () => {
-  it('runs logout cmd', async () => {
-    const email = await runCommand('logout')
-    expect(email.stdout).to.contain('Not logged in.')
-  })
-})
--- test/commands/org/switch.test.ts
+++
@@ -1,9 +0,0 @@
-import {runCommand} from '@oclif/test'
-import {expect} from 'chai'
-
-describe('org:switch', () => {
-  it('runs org:switch cmd', async () => {
-    const {stdout} = await runCommand('org:switch')
-    expect(stdout).to.contain('Not logged in.')
-  })
-})
