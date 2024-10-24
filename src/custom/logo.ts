9aa0c87abf47a27e14b4eaa2f72604ace810c2e1
Jai Radhakrishnan <55522316+jairad26@users.noreply.github.com>
2024-10-24 10:35:09 -0700

    prettify logo (#16)

---
+++ src/custom/logo.ts
@@ -0,0 +1,22 @@
+/*
+ * Copyright 2024 Hypermode Inc.
+ * Licensed under the terms of the Apache License, Version 2.0
+ * See the LICENSE file that accompanied this code for further details.
+ *
+ * SPDX-FileCopyrightText: 2024 Hypermode Inc. <hello@hypermode.com>
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import gradient from 'gradient-string'
+
+const logo = String.raw`
+     __ __                                __   
+    / // /_ _____  ___ ______ _  ___  ___/ /__ 
+   / _  / // / _ \/ -_) __/  ' \/ _ \/ _  / -_)
+  /_//_/\_, / .__/\__/_/ /_/_/_/\___/\_,_/\__/ 
+       /___/_/                                 
+`
+
+export function getLogo(): string {
+  return gradient(['#E3BFFF', '#602AF8']).multiline(logo)
+}
